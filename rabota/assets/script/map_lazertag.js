daa27c27a9f5d596f1b3b2a35794c503aefd624e
Kristina <shust1991@mail.ru>
2017-12-29 10:39:55 +0000

    на проверку

--- rabota/assets/script/map_lazertag.js
+++ rabota/assets/script/map_lazertag.js
@@ -1,56 +1,55 @@
 function initMap() {
-   
-
+    
     var centerLatLng = new google.maps.LatLng(53.904390, 27.559782);
     var mapOptions = {
         center: centerLatLng,
         zoom: 8
-    };
-    var myLatLng = { lat: 54.160205, lng: 27.834909 };
-    var myLatLng2 = { lat: 54.059220, lng: 27.734622 };
-    var myLatLng3 = { lat:54.181853, lng: 27.807299 };
-    var myLatLng4 = { lat: 53.846617, lng: 27.514897 };
-    var myLatLng5 = { lat: 53.821473,  lng: 27.019018};
+    };  
+    var myLatLng1 = { lat:53.964113, lng: 27.653549 };
+    var myLatLng2 = { lat: 53.789416, lng: 27.505395 };
+    var myLatLng3 = { lat: 53.943102, lng: 27.330802 };
+    var myLatLng4 = {lat: 53.975386, lng: 27.288496 };
+    var myLatLng5 = { lat: 53.944000,   lng: 27.328435};
 
-    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+    var map2 = new google.maps.Map(document.getElementById("mapLazer"), mapOptions);
 
 
 
     var marker = new google.maps.Marker({
         position: myLatLng,
-        map: map,
+        map: map2,
         label: '1',
         animation: google.maps.Animation.DROP,
-        title: 'Силичи',
+        title: 'UFO',
         
     });
     var marker2 = new google.maps.Marker({
         position: myLatLng2,
-        map: map,
+        map: map2,
         label: '2',
         animation: google.maps.Animation.DROP,
-        title: 'Раубичи'
+        title: 'Клуб 067'
     });
     var marker3 = new google.maps.Marker({
         position: myLatLng3,
-        map: map,
+        map: map2,
         label: '3',
         animation: google.maps.Animation.DROP,
-        title: 'Логойск'
+        title: 'Альянс'
     });
     var marker4 = new google.maps.Marker({
         position: myLatLng4,
-        map: map,
+        map: map2,
         label: '4',
         animation: google.maps.Animation.DROP,
-        title: 'Солнечная долина'
+        title: 'Laser Battle'
     });
     var marker5 = new google.maps.Marker({
         position: myLatLng5,
-        map: map,
+        map: map2,
         label: '5',
         animation: google.maps.Animation.DROP,
-        title: 'Якутские горы'
+        title: 'Colt'
     });
     
     
