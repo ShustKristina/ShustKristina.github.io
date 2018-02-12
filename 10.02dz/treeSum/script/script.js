6178bb89b9993300a2c2617ebcc0883b0c9dbc48
Kristina <shust1991@mail.ru>
2018-02-12 15:21:10 +0000

    correct

--- 10.02dz/treeSum/script/script.js
+++ 10.02dz/treeSum/script/script.js
@@ -1,21 +1,25 @@
  var test = [5, 7, [4, [2], 8, [1, 3], 2],
-   [9, []],
-   1, 8
+  [9, []],
+  1, 8
  ];
 
  var test2 = [];
 
  function treeSum(arr) {
 
-   var sum = 0; //Сумме присваивается значение 0
-   arr.forEach(function(v) { //Метод forEach() выполняет указанную функцию один раз для каждого элемента в массиве
+  var sum = 0; //Сумме присваивается значение 0
+  arr.forEach(function(v) { //Метод forEach() выполняет указанную функцию один раз для каждого элемента в массиве
 
-     if (typeof v != 'object') // Условие остановки рекурсии
-       return sum += v;
-     return sum += treeSum(v); // Рекурсия 
-   });
+   if (typeof v != 'object') { // Условие остановки рекурсии
+    sum += v;
+   }
+   else {
 
-   return sum; // Возврат рез-та
+    sum += treeSum(v); // Рекурсия 
+   }
+  });
+
+  return sum; // Возврат рез-та
  }
  console.log(treeSum(test)); // Вывод суммы элементов тестового массива в консоль
  console.log(treeSum(test2));
