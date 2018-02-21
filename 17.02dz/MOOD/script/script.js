ca923054078dd275c6079c93f13088e5f9582d40
Kristina <shust1991@mail.ru>
2018-02-21 12:34:10 +0000

    unrepeating colors

--- 17.02dz/MOOD/script/script.js
+++ 17.02dz/MOOD/script/script.js
@@ -3,25 +3,26 @@
   }
 
   function mood(colorsCount) {
-      var colorHash = {};
       var colors = ['', 'красный', 'оранжевый', 'жёлтый', 'зелёный', 'голубой', 'синий', 'фиолетовый'];
+      var colorHash = {};
       console.log('цветов: ' + colorsCount);
-      while (Object.keys(colorHash).length < colorsCount) {
-          do {
-              var n = randomDiap(1, 7); 
-              var unicColor = true; //цвет встречается впервые
 
-              for (var i = 0; i < colorsCount; i++) {
-                  var colorName = colors[n];
-                  if (colorName == colorHash[i]) {  //проверка, есть ли случайно выбранный цвет в хэше
-                      unicColor = false;  //если да, то это не уникальный цвет
-                      break; //прекращение выполнения цикла
-                  }
-                   colorHash[colorName] = 1; //если нет, то происходит добавление цвета в хэш
+      while (Object.keys(colorHash).length < colorsCount) { 
+
+          var n = randomDiap(1, 7);
+          var colorName = colors[n];
+
+          for (var i = 1; i <= colorsCount; i++) {
+              if (colorName in colorHash) {
+                  continue;
               }
-          } while (!unicColor) 
+              else {
+                  colorHash[colorName] = true;
+
+              }
+              console.log(colorName);
+          }
       }
-      console.log(colorHash);
   }
 
   mood(3);
