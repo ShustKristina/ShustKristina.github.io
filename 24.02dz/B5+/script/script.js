ef5832fd40260dfe614e0bc261f44001ad34945f
Kristina <shust1991@mail.ru>
2018-03-03 17:53:16 +0000

    for string operations

--- 24.02dz/B5+/script/script.js
+++ 24.02dz/B5+/script/script.js
@@ -1,22 +1,19 @@
-/*
-Напишите функцию для оборачивания текста в тег, с которой можно было
-бы работать в следующем стиле:
-var wrapH1=buildWrapper("H1"); // строим функцию для оборачивания текста в тег H1
-var wrapP=buildWrapper("P");   // строим функцию для оборачивания текста в тег P
-console.log( wrapH1("СТИХИ") );
-// в консоль выводится строка "<H1>СТИХИ</H1>"
-console.log( wrapP("Однажды в студёную зимнюю пору") );
-// в консоль выводится строка "<P>Однажды в студёную зимнюю пору</P>"
-Функция должна учитывать, что некоторые символы надо замеменять на HTML-мнемоники:
-console.log( wrapP("Вкусные M&M's") );
-// в консоль выводится строка "<P>Вкусные M&amp;M&apos;s</P>"
+// не работает
+function buildWrapper(arg) {
+    return (function buildWrapper(arg2) {
+        var element = document.createElement(arg); //create tag
+        element.innerHTML = arg2; //paste the text
+        var result = document.body.appendChild(element); //text+tag
+        return result;
+    });
+}
 
-
-var wrapH1=buildWrapper("H1"); buildWrapper - это функция, и она должна вернуть ФУНКЦИЮ...
-console.log( wrapH1("СТИХИ") ); ...которая получает текст и возвращает этот же текст, обёрнутый в теги
-
-*/
-
+//test
+var wrapH1 = buildWrapper("H1");
+var wrapP = buildWrapper("P");
+console.log(wrapH1("СТИХИ"));
+console.log(wrapP("Однажды в студёную зимнюю пору"));
+console.log(wrapP("Вкусные M&M's"));
  
 
 
