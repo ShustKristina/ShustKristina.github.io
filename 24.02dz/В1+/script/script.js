6c69f324d2d58aa52d774b06c57197a046740914
Kristina <shust1991@mail.ru>
2018-03-16 19:22:14 +0000

    Correct B1

--- 24.02dz/В1+/script/script.js
+++ 24.02dz/В1+/script/script.js
@@ -1,9 +1,5 @@
 function convertYearInCentury(arg) {
-    if (arg % 100 == 0) {
-        return arg / 100;
-    } else {
-        return Math.floor(arg / 100 + 1);
-    }
+    return (arg % 100 == 0)?arg / 100:Math.floor(arg / 100 + 1);  
 }
-var test = +prompt("Введите год", "2000");
+var test = +prompt("Введите год", "1875");
 alert("Это " + convertYearInCentury(test) + " век");
