a1f9b00d39051853346d96fbaed28ecd949e076d
Kristina <shust1991@mail.ru>
2018-03-18 18:36:52 +0000

    B1

--- 24.02dz/В1+/script/script.js
+++ 24.02dz/В1+/script/script.js
@@ -1,5 +1,5 @@
 function convertYearInCentury(arg) {
-    return (arg % 100 == 0)?arg / 100:Math.floor(arg / 100 + 1);  
+    return Math.ceil(arg / 100);  
 }
 var test = +prompt("Введите год", "1875");
 alert("Это " + convertYearInCentury(test) + " век");
