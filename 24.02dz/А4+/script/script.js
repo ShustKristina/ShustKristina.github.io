f37cced39f95ef03ceba895fd98ab70bc0ca6534
Kristina <shust1991@mail.ru>
2018-03-22 17:39:19 +0000

    correct palindrom

--- 24.02dz/А4+/script/script.js
+++ 24.02dz/А4+/script/script.js
@@ -1,12 +1,18 @@
 function palindrome(str) {
-    str = str.toLowerCase().trim().replace(".", "").replace(",", "").replace(":", "").replace(";", "").replace("!", "").replace("?", "").replace("ё", "е").replace("ъ", "ь").replace("-", ""); 
-    var l = str.length < 2; 
-    var letterEqual = (str.slice(0, 1) === str.slice(-1)); 
-    return !l && letterEqual ? palindrome(str.slice(1, -1)) : l;
+    str = str.toLowerCase().replace(".", "").replace(",", "").replace(":", "").replace(";", "").replace("!", "").replace("?", "").replace("ё", "е").replace("ъ", "ь").replace("-", "").trim();
+    var strNew = str;
+
+    if (strNew.length <= 1) {
+        return true;
+    }
+    if (strNew.slice(0, 1) != strNew.slice(-1)) {
+        return false;
+    }
+
+    return palindrome(strNew.slice(1, -1));
 }
 
-
-var test = "мама мыла раму";
+var test = "иа";
 var test2 = "!еА роза упала, на лапу. азораЁ";
 console.log(palindrome(test));
 console.log(palindrome(test2));
