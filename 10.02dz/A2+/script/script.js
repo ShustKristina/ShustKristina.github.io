dc1aff914c1557ba9dbf6dbed6bceca7d34eba50
Kristina <shust1991@mail.ru>
2018-03-22 15:56:41 +0000

    trim

--- 10.02dz/A2+/script/script.js
+++ 10.02dz/A2+/script/script.js
@@ -1,45 +1,17 @@
 var test = prompt('Введите текст');
 
-function removeSpace(arg) {
- var l = arg.length;
- var strRemoveSpace = '';
- var c, c0 = ' ';
-
- for (var i = 0; i < l; i++) {
-  c = arg.substring(i, i + 1);
-  if (c == ' ') {
-   if (c0 == ' ')
-    continue;
-  }
-  c0 = c;
-  strRemoveSpace += c;
- }
-l = strRemoveSpace.length;
- if (strRemoveSpace.substring(l - 1, l) == ' ') {
-  strRemoveSpace = strRemoveSpace.substring(0, l - 1);
- }
- else {
-  return strRemoveSpace;
- }
+function trim(str) {
+    str = str.split('');
+    for (var i = 0; i < str.length; i++) {
+        if (str[0] == " ") {
+            str.shift();
+        }
+    }
+    for (var j = str.length - 1; j >= 0; j--) {
+        if (str[str.length - 1] == " ") {
+            str.pop();
+        }
+    }
+    return str.join(" ");
 }
-console.log("<" + removeSpace(test) + ">");
-
-function trim(s) {
- var l = s.length;
- var t = '',
-  c, c0 = ' ';
- for (var i = 0; i < l; i++) {
-  c = s.substring(i, i + 1);
-  if (c == ' ') {
-   if (c0 == ' ')
-    continue;
-  }
-  c0 = c;
-  t += c;
- }
- l = t.length;
- if (t.substring(l - 1, l) == ' ') t = t.substring(0, l - 1);
- return t;
-}
-
 console.log("<" + trim(test) + ">");
