b750d2bff4aae20c2e467b18e450dd4dbe02217b
Kristina <shust1991@mail.ru>
2018-03-23 08:23:41 +0000

    string trim

--- 10.02dz/A2+/script/script.js
+++ 10.02dz/A2+/script/script.js
@@ -1,17 +1,23 @@
 var test = prompt('Введите текст');
 
 function trim(str) {
-    str = str.split('');
-    for (var i = 0; i < str.length; i++) {
-        if (str[0] == " ") {
-            str.shift();
-        }
+    var spaceStart = 0,
+        spaceEnd = 0,
+        i = 0,
+        j = str.length - 1;
+
+    while (str[i] == " ") {
+        spaceStart++;
+        i++;
     }
-    for (var j = str.length - 1; j >= 0; j--) {
-        if (str[str.length - 1] == " ") {
-            str.pop();
-        }
+
+    while (str[j] == " ") {
+        spaceEnd++;
+        j--;
     }
-    return str.join(" ");
+    return str.substring(spaceStart, str.length - spaceEnd);
+    
+    //можно еще так записать return str.substring(i, j+1);
+    //тогда переменные spaceStart, spaceEnd вообще не нужны будут
 }
 console.log("<" + trim(test) + ">");
