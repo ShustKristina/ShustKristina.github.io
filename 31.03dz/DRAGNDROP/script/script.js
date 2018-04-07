83788a3fa22f4945726dc5d97c56dfe11f52fe3b
Kristina <shust1991@mail.ru>
2018-04-07 17:03:13 +0000

    correct

--- 31.03dz/DRAGNDROP/script/script.js
+++ 31.03dz/DRAGNDROP/script/script.js
@@ -2,10 +2,28 @@
 var container = document.querySelector(".container");
 var images = container.querySelectorAll("img");
 
-//Для каждой картинки добавляем событие, возникающее при нажатии на левую клавишу мыши
-for (var i = 0; i < images.length; i++) {
-    images[i].style.cursor = "pointer";
-    images[i].addEventListener("mousedown", MouseEvents);
+//Для каждой картинки добавляем событие, возникающее при нажатии на левую клавишу мыши, находим первоначальные координаты изображений
+
+for (var j = images.length - 1; j >= 0; j--) {
+    images[j].style.cursor = "pointer";
+    images[j].addEventListener("mousedown", MouseEvents);
+    images[j].style.position = "absolute";
+    images[j].style.left = GetElementPos(images[j]).left + "px";
+    images[j].style.top = GetElementPos(images[j]).top + "px";
+}
+
+function GetElementPos(EL) {
+    var X = 0;
+    var Y = 0;
+    while (EL) {
+        X += EL.offsetLeft;
+        Y += EL.offsetTop;
+        EL = EL.offsetParent;
+    }
+    return {
+        left: X,
+        top: Y
+    };
 }
 
 function MouseEvents(event) {
@@ -14,22 +32,22 @@ function MouseEvents(event) {
     dragElement.ondragstart = function () {
         return false;
     };
-    //Получаем координаты, позиционируем элемент
+    //Определение расстояния, на которое сдвинут курсор относительно левого верхнего угла изображения
+    //Выполнение условия, что последняя перетаскиваемая картинка оказывается выше остальных
     function ElementPosition(pageX, pageY) {
-        X = pageX - dragElement.getBoundingClientRect().left;
-        Y = pageY - dragElement.getBoundingClientRect().top;
-        dragElement.style.position = "absolute";
+        shiftX = pageX - dragElement.getBoundingClientRect().left;
+        shiftY = pageY - dragElement.getBoundingClientRect().top;
         document.body.appendChild(dragElement);
         moveAt(pageX, pageY);
     };
-
+    ElementPosition(event.pageX, event.pageY);
+    //Определение координат места, куда перетаскивается изображение
     function moveAt(pageX, pageY) {
-        var newX = pageX - X;
-        var newY = pageY - Y;
+        var newX = pageX - shiftX;
+        var newY = pageY - shiftY;
         dragElement.style.left = newX + "px";
         dragElement.style.top = newY + "px";
     }
-    ElementPosition(event.pageX, event.pageY);
     //останавливаем перенос элемента при отпускании левой кнопки мыши
     document.onmouseup = function finishMouseMove() {
         document.onmousemove = null;
