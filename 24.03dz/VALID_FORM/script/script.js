6284c0e85bd3aca88fbfe138729f2f35f4139d18
Kristina <shust1991@mail.ru>
2018-04-09 19:19:06 +0000

    divError in html

--- 24.03dz/VALID_FORM/script/script.js
+++ 24.03dz/VALID_FORM/script/script.js
@@ -15,22 +15,22 @@ function validDevelopers(autoFocus) {
     var reg1 = /[а-яА-Я]/;
     var error = document.querySelector('.errorDev');
     if (developValue == "" || !reg1.test(developValue)) { //регулярные выражения знаю плохо, поэтому записала условие так, как смогла
-        error.style.color = 'red'
+        error.style.color = 'red';
         error.innerHTML = 'Поле не может быть пустым, содержать цифры и латинские буквы!';
         if (autoFocus) {
             develop.focus();
         }
         return true;
     } else {
-        error.style.color = 'green'
-        error.innerHTML = "&#10004;"
+        error.style.color = 'green';
+        error.innerHTML = "&#10004;";
         develop.style.border = "1px solid green";
         return false;
     }
 }
 
 develop.addEventListener("blur", function () {
-    validDevelopers(false)
+    validDevelopers(false);
 });
 
 function validNameSite(autoFocus) {
@@ -38,7 +38,7 @@ function validNameSite(autoFocus) {
     var reg1 = /[а-яА-Я]/;
     var error = document.querySelector('.errorName');
     if (nameSiteValue == "" || !reg1.test(nameSiteValue)) {
-        error.style.color = 'red'
+        error.style.color = 'red';
         error.innerHTML = 'Поле не может быть пустым, содержать цифры и латинские буквы!';
         if (autoFocus) {
             nameSite.focus();
@@ -46,13 +46,13 @@ function validNameSite(autoFocus) {
         return true;
     } else {
         error.style.color = 'green';
-        error.innerHTML = "&#10004;"
+        error.innerHTML = "&#10004;";
         nameSite.style.border = "1px solid green";
         return false;
     }
 }
 nameSite.addEventListener("blur", function () {
-    validNameSite(false)
+    validNameSite(false);
 });
 
 function validAddressSite(autoFocus) {
@@ -60,7 +60,7 @@ function validAddressSite(autoFocus) {
     var reg3 = /^https:/;
     var error = document.querySelector('.errorURL');
     if (addressSiteValue == "" || !reg3.test(addressSiteValue)) {
-        error.style.color = 'red'
+        error.style.color = 'red';
         error.innerHTML = 'Поле не может быть пустым и URL начинается с https:!';
         if (autoFocus) {
             addressSite.focus();
@@ -68,14 +68,14 @@ function validAddressSite(autoFocus) {
         return true;
     } else {
         error.style.color = 'green';
-        error.innerHTML = "&#10004;"
+        error.innerHTML = "&#10004;";
         addressSite.style.border = "1px solid green";
         return false;
     }
 }
 
 addressSite.addEventListener("blur", function () {
-    validAddressSite(false)
+    validAddressSite(false);
 });
 
 function validDate(autoFocus) {
@@ -84,7 +84,7 @@ function validDate(autoFocus) {
     var error = document.querySelector('.errorDate');
 
     if (dateValue == "" || !reg4.test(dateValue)) {
-        error.style.color = 'red'
+        error.style.color = 'red';
         error.innerHTML = 'Поле не может быть пустым, введите число!';
         if (autoFocus) {
             date.focus();
@@ -92,7 +92,7 @@ function validDate(autoFocus) {
         return true;
     } else {
         error.style.color = 'green';
-        error.innerHTML = "&#10004;"
+        error.innerHTML = "&#10004;";
         date.style.border = "1px solid green";
         return false;
     }
@@ -107,7 +107,7 @@ function validVisitors(autoFocus) {
     var error = document.querySelector('.errorVisitors');
 
     if (visitorsValue == "" || !reg4.test(visitorsValue)) {
-        error.style.color = 'red'
+        error.style.color = 'red';
         error.innerHTML = 'Поле не может быть пустым, введите цифры!';
         if (autoFocus) {
             visitors.focus();
@@ -115,13 +115,13 @@ function validVisitors(autoFocus) {
         return true;
     } else {
         error.style.color = 'green';
-        error.innerHTML = "&#10004;"
+        error.innerHTML = "&#10004;";
         visitors.style.border = "1px solid green";
         return false;
     }
 }
 visitors.addEventListener("blur", function () {
-    validVisitors(false)
+    validVisitors(false);
 });
 
 function validEmail(autoFocus) {
@@ -130,7 +130,7 @@ function validEmail(autoFocus) {
     var error = document.querySelector('.errorEmail');
 
     if (emailValue == "" || !reg5.test(emailValue)) {
-        error.style.color = 'red'
+        error.style.color = 'red';
         error.innerHTML = 'Поле не может быть пустым, название почты должно оканчиваться на @gmail.com!';
         if (autoFocus) {
             email.focus();
@@ -138,22 +138,21 @@ function validEmail(autoFocus) {
         return true;
     } else {
         error.style.color = 'green';
-        error.innerHTML = "&#10004;"
+        error.innerHTML = "&#10004;";
         email.style.border = "1px solid green";
         return false;
     }
 }
 email.addEventListener("blur", function () {
-    validEmail(false)
+    validEmail(false);
 });
 
 function validCatalog(autoFocus) {
     var catalogValue = catalog.value;
-    var reg4 = /[0-9]/;
     var error = document.querySelector('.errorCatalog');
 
     if (catalogValue == 1) {
-        error.style.color = 'red'
+        error.style.color = 'red';
         error.innerHTML = 'Выберите другой вариант!';
         if (autoFocus) {
             catalog.focus();
@@ -161,20 +160,20 @@ function validCatalog(autoFocus) {
         return true;
     } else {
         error.style.color = 'green';
-        error.innerHTML = "&#10004;"
+        error.innerHTML = "&#10004;";
         catalog.style.border = "1px solid green";
         return false;
     }
 }
 catalog.addEventListener("change", function () {
-    validCatalog(false)
+    validCatalog(false);
 });
 
 function validCost(autoFocus) {
     var error = document.querySelector('.errorCost');
 
-    if (cost.value == "") {
-        error.style.color = 'red'
+    if (!cost[0].checked && !cost[1].checked && !cost[2].checked) {
+        error.style.color = 'red';
         error.innerHTML = 'Выберите тип размещения!';
         if (autoFocus) {
             document.getElementsByClassName("cost1").scrollIntoView();
@@ -182,21 +181,23 @@ function validCost(autoFocus) {
         return true;
     } else {
         error.style.color = 'green';
-        error.innerHTML = "&#10004;"
+        error.innerHTML = "&#10004;";
         date.style.border = "1px solid green";
         return false;
     }
 }
 
-cost[0].addEventListener("change", function () {
-    validCost(false)
-});
+for (var i = 0; i < cost.length; i++) {
+    cost[i].addEventListener("change", function () {
+        validCost(false);
+    });
+}
 
 function validReview(autoFocus) {
     var error = document.querySelector('.errorReview');
 
     if (!review.checked) {
-        error.style.color = 'red'
+        error.style.color = 'red';
         error.innerHTML = 'Разрешите оставлять отзывы!';
         if (autoFocus) {
             review.focus();
@@ -204,13 +205,13 @@ function validReview(autoFocus) {
         return true;
     } else {
         error.style.color = 'green';
-        error.innerHTML = "&#10004;"
+        error.innerHTML = "&#10004;";
         review.style.border = "1px solid green";
         return false;
     }
 }
 review.addEventListener("change", function () {
-    validReview(false)
+    validReview(false);
 });
 
 function validDescription(autoFocus) {
@@ -218,7 +219,7 @@ function validDescription(autoFocus) {
     var error = document.querySelector('.errorDescription');
 
     if (!descriptionValue || descriptionValue.length > 1200) {
-        error.style.color = 'red'
+        error.style.color = 'red';
         error.innerHTML = 'Опишите сайт (допустимо не более 1200 символов)!';
         if (autoFocus) {
             description.focus();
@@ -226,24 +227,22 @@ function validDescription(autoFocus) {
         return true;
     } else {
         error.style.color = 'green';
-        error.innerHTML = "&#10004;"
+        error.innerHTML = "&#10004;";
         description.style.border = "1px solid green";
         return false;
     }
 }
 description.addEventListener("blur", function () {
-    validDescription(false)
+    validDescription(false);
 });
 
 form.addEventListener("submit", validAllFields, true);
 
 function validAllFields(EO) {
     EO = EO || window.event;
-    EO.preventDefault();
-
     try {
         var totalErrCount = 0;
-        totalErrCount += validDevelopers(!totalErrCount); // автофокус разрешаем только если ошибок пока нет ни одной, т.е. только для самого первого ошибочного поля
+        totalErrCount += validDevelopers(!totalErrCount); 
         totalErrCount += validNameSite(!totalErrCount);
         totalErrCount += validAddressSite(!totalErrCount);
         totalErrCount += validDate(!totalErrCount);
@@ -254,8 +253,8 @@ function validAllFields(EO) {
         totalErrCount += validReview(!totalErrCount);
         totalErrCount += validDescription(!totalErrCount);
         if (totalErrCount)
-            EO.preventDefault(); // если ошибки были - отменяем отправку формы на сервер
+            EO.preventDefault(); 
     } catch (err) {
-        EO.preventDefault(); // что-то пошло не так - отменяем отправку формы на сервер
+        EO.preventDefault(); 
     }
 }
