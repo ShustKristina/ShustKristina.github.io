577215ade40b4eb33490252d21b6040a14105892
ShustKristina <shust1991@mail.ru>
2018-04-28 10:51:37 +0300

    tag+attributes+text

---
+++ 21.04dz/B6+/script/script.js
@@ -0,0 +1,30 @@
+function buildWrapper(arg) {
+    return function buildWrapper(arg2, arg3) {
+        var tag = arg; //create tag
+        var text = arg2.split('&').join("&amp;").split("'").join("&apos;").split('"').join("&quot;").split('<').join("&lt;").split('>').join("&gt;"); //paste the text
+        if (arg3) {
+            var attributesArr = [];
+            for (var key in arg3) {
+                var attr = key + '=' + '"' + arg3[key] + '"';
+                attributesArr.push(attr);
+            }
+            var result = "<" + tag + " " + attributesArr + ">" + text + "<" + "/" + tag + ">"; //text+tag+attributes
+        }
+        else {
+            var result = "<" + tag + ">" + text + "<" + "/" + tag + ">"; //text+tag 
+        }
+        return result;
+    };
+}
+
+//test
+var wrapH1 = buildWrapper("H1");
+var wrapP = buildWrapper("P");
+console.log(wrapH1("СТИХИ", { align: "center", color: "red", border: "1px solid red"}));
+console.log(wrapP("Однажды в <студёную> зимнюю пору"));
+console.log(wrapP("Вкусные M&M's"));
+
+
+
+
+
