739e88130fc1539ff9d05ec0d0b8a189d6703a55
ShustKristina <shust1991@mail.ru>
2018-05-05 18:22:45 +0300

    clock

--- 29.04dz/CLOCK_CANVAS/script/script.js
+++ 29.04dz/CLOCK_CANVAS/script/script.js
@@ -12,81 +12,100 @@ var widthSecondsHand = 0.2 * widthHoursHand; // ширина секундной 
 var heightSecondsHand = 0.9 * radiusYellowCircle; //высота секундной стрелки=216
 var positionTopTime = 0.5 * radiusYellowCircle;
 
-var canvasElem = document.getElementById("clock");
-var context = canvasElem.getContext('2d');
-
-function displayClock() {
-    //create clockFace
-    context.fillStyle = 'yellow';
-    context.beginPath();
-    var clockFaceCenterX = radiusYellowCircle;
-    var clockFaceCenterY = clockFaceCenterX;
-    context.arc(clockFaceCenterX, clockFaceCenterY, radiusYellowCircle, 0, Math.PI * 2, false);
-    context.fill();
-
-    //create green circles
-    for (var i = 1, number = 12; i <= number; i++) {
-        context.beginPath();
-        var angle = 2 / number * i * Math.PI;
-        var greenCircleCenterX = clockFaceCenterX + distance * Math.sin(angle);
-        var greenCircleCenterY = clockFaceCenterY - distance * Math.cos(angle);
-        context.fillStyle = 'green';
-        context.arc(greenCircleCenterX, greenCircleCenterY, radiusGreenCircle, 0, Math.PI * 2, false);
-        context.fill();
-        //create numbers
-        context.fillStyle = 'black';
-        context.font = fontSizeNumbers + "px Arial";
-        context.textAlign = 'center';
-        context.textBaseline = 'middle';
-        context.fillText(i, greenCircleCenterX, greenCircleCenterY);
-    }
-
-    //create hands
-    var currTime = new Date();
-    //angles
-    var sec = 6 * currTime.getSeconds();
-    var min = 6 * currTime.getMinutes();
-    var hour = 30 * currTime.getHours();
-
-    //create hour hand
-    context.beginPath();
-    context.moveTo(clockFaceCenterX, clockFaceCenterY);
-    context.lineTo(clockFaceCenterX + heightHoursHand * Math.cos(Math.PI / 2 - hour * (Math.PI / 180)),
-        clockFaceCenterY - heightHoursHand * Math.sin(Math.PI / 2 - hour * (Math.PI / 180)));
-    context.lineWidth = widthHoursHand;
-    context.lineCap = 'round';
-    context.strokeStyle = "black";
-    context.stroke();
-
-    //create minute hand
-    context.beginPath();
-    context.moveTo(clockFaceCenterX, clockFaceCenterY);
-    context.lineTo(clockFaceCenterX + heightMinutesHand * Math.cos(Math.PI / 2 - min * (Math.PI / 180)),
-        clockFaceCenterY - heightMinutesHand * Math.sin(Math.PI / 2 - min * (Math.PI / 180)));
-    context.lineWidth = widthMinutesHand;
-    context.lineCap = 'round';
-    context.strokeStyle = "black";
-    context.stroke();
-
-    //create second hand
-    context.beginPath();
-    context.moveTo(clockFaceCenterX, clockFaceCenterY);
-    context.lineTo(clockFaceCenterX + heightSecondsHand * Math.cos(Math.PI / 2 - sec * (Math.PI / 180)),
-        clockFaceCenterY - heightSecondsHand * Math.sin(Math.PI / 2 - sec * (Math.PI / 180)));
-    context.lineWidth = widthSecondsHand;
-    context.lineCap = 'round';
-    context.strokeStyle = "black";
-    context.stroke();
-
-    //current time
-    context.fillStyle = 'black';
-    context.font = fontSizeTime + "px Arial";
-    context.textAlign = 'center';
-    context.fillText(currTime.toLocaleTimeString(), radiusYellowCircle, positionTopTime);
-}
-
 function Clock() {
     displayClock();
 }
 setInterval(Clock, 1000);
 Clock();
+
+function displayClock() {
+    var currTime = new Date();
+    var seconds = currTime.getSeconds();
+    var minutes = currTime.getMinutes();
+    var hours = currTime.getHours();
+    var hour = hours + minutes / 60;
+    var minute = minutes + seconds / 60;
+    var canvas = document.getElementById('clock');
+    var ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, 500, 500);
+
+    clockFace();
+    greenCirclesAndNumbers()
+    secondsHand();
+    minutesHand();
+    hoursHand();
+    currentTime();
+
+    function clockFace() {
+        var clockFaceCenterX = radiusYellowCircle;
+        var clockFaceCenterY = clockFaceCenterX;
+        ctx.fillStyle = 'yellow';
+        ctx.beginPath();
+        ctx.arc(clockFaceCenterX, clockFaceCenterY, radiusYellowCircle, 0, Math.PI * 2);
+        ctx.fill();
+    }
+
+    function greenCirclesAndNumbers() {
+        for (var i = 1, number = 12; i <= number; i++) {
+            ctx.beginPath();
+            var angle = 2 / number * i * Math.PI;
+            var greenCircleCenterX = radiusYellowCircle + 0.8 * radiusYellowCircle * Math.sin(angle);
+            var greenCircleCenterY = radiusYellowCircle - 0.8 * radiusYellowCircle * Math.cos(angle);
+            ctx.fillStyle = 'green';
+            ctx.arc(greenCircleCenterX, greenCircleCenterY, radiusGreenCircle, 0, Math.PI * 2, false);
+            ctx.fill();
+            //create numbers
+            ctx.fillStyle = 'black';
+            ctx.font = fontSizeNumbers + "px Arial";
+            ctx.textAlign = 'center';
+            ctx.textBaseline = 'middle';
+            ctx.fillText(i, greenCircleCenterX, greenCircleCenterY);
+        }
+    }
+
+    function secondsHand() {
+        var angle = (seconds - 15) * 2 * Math.PI / 60;
+        ctx.lineWidth = widthSecondsHand;
+        ctx.beginPath();
+        ctx.moveTo(radiusYellowCircle, radiusYellowCircle);
+        ctx.lineTo((radiusYellowCircle + Math.cos(angle) * heightSecondsHand), radiusYellowCircle + Math.sin(angle) * heightSecondsHand);
+        ctx.moveTo(radiusYellowCircle, radiusYellowCircle);
+        ctx.lineTo((radiusYellowCircle - Math.cos(angle) * 15), radiusYellowCircle - Math.sin(angle) * 15);
+        ctx.lineCap = 'round';
+        ctx.strokeStyle = 'black';
+        ctx.stroke();
+    }
+
+    function minutesHand() {
+        angle = (minute - 15) * 2 * Math.PI / 60;
+        ctx.lineWidth = widthMinutesHand;
+        ctx.beginPath();
+        ctx.moveTo(radiusYellowCircle, radiusYellowCircle);
+        ctx.lineTo((radiusYellowCircle + Math.cos(angle) * heightMinutesHand), radiusYellowCircle + Math.sin(angle) * heightMinutesHand);
+        ctx.moveTo(radiusYellowCircle, radiusYellowCircle);
+        ctx.lineTo((radiusYellowCircle - Math.cos(angle) * 15), radiusYellowCircle - Math.sin(angle) * 15);
+        ctx.lineCap = 'round';
+        ctx.strokeStyle = 'black';
+        ctx.stroke();
+    }
+
+    function hoursHand() {
+        angle = (hour - 3) * 2 * Math.PI / 12;
+        ctx.lineWidth = widthHoursHand;
+        ctx.beginPath();
+        ctx.moveTo(radiusYellowCircle, radiusYellowCircle);
+        ctx.lineTo((radiusYellowCircle + Math.cos(angle) * heightHoursHand), radiusYellowCircle + Math.sin(angle) * heightHoursHand);
+        ctx.moveTo(radiusYellowCircle, radiusYellowCircle);
+        ctx.lineTo((radiusYellowCircle - Math.cos(angle) * 15), radiusYellowCircle - Math.sin(angle) * 15);
+        ctx.strokeStyle = 'black';
+        ctx.lineCap = 'round';
+        ctx.stroke();
+    }
+
+    function currentTime() {
+        ctx.fillStyle = 'black';
+        ctx.font = fontSizeTime + "px Arial";
+        ctx.textAlign = 'center';
+        ctx.fillText(currTime.toLocaleTimeString(), radiusYellowCircle, positionTopTime);
+    }
+}
