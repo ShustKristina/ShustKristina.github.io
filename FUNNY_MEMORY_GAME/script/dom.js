4ae83c1b4937d8ce23980ceb8fd096dbb0a11a6c
ShustKristina <shust1991@mail.ru>
2018-05-17 09:13:02 +0300

    ajax

--- FUNNY_MEMORY_GAME/script/dom.js
+++ FUNNY_MEMORY_GAME/script/dom.js
@@ -167,6 +167,8 @@ function Game() {
                         if (document.querySelectorAll(".hidden").length === self.numberOfCards) {
 
                             timer1.stop();
+                            storeInfo()
+restoreInfo();
                             self.flowingCongratulations();
                         }
 
