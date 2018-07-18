3d526f26c461524a566abb23511a368294c0364d
Krystina Shust <kris@ws000043.gurtam.net>
2018-07-18 11:21:28 +0300

    geo_test

--- Geocode/script/parsing.js
+++ Geocode/script/parsing.js
@@ -25,7 +25,38 @@ var array=new Array();
         console.log(array);
         array.length=10;
         console.log(array);
-        
+for (var i=0; i<array.length; i++){
+    function saveCircle(){ // create circle geofence using entered data
+        // construct object using entered data
+        var res = 17634994;
+        var obj = { n: ""+array[i].Название+"", //geofence name
+            t: 3, // type (3 - circle)
+            f: 0, // flags
+            w: 700, // radius
+            c: 2566914048, // color
+            p: [{x: array[i].Широта, y: array[i].Долгота, r: 700}] // points (for circle, center coords and radius)
+        };
+    
+        var resource = wialon.core.Session.getInstance().getItem(res); // get resource by id
+        resource.createZone(obj, // create geofence
+            function(code, data){ // create geofence callback
+                if(code){ msg(wialon.core.Errors.getErrorText(code)); return; } // exit if error code
+                msg("<b>'"+data.n+"'</b> geofence created successfully"); // print create succeed message
+            });
+    }
+
+    $(document).ready(function () {
+        saveCircle()
+    
+        wialon.core.Session.getInstance().initSession("https://hst-api.wialon.com"); // init session
+
+        wialon.core.Session.getInstance().loginToken("9a199bdb98a4ac5d147cfd84cfed1047EE6C6BEB363CE635FC5C0E5BD519910BDB440E62", "", // try to login
+            function (code) { // login callback
+                if (code){ msg(wialon.core.Errors.getErrorText(code)); return; } // exit if error code
+        });
+    });
+}
+     
 }
 oReq.send();
 
